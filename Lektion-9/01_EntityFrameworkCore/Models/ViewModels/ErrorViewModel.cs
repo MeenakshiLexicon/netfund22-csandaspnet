555907cb7d9ebabc0f9a3e5cf9d04d54300325c1
MeenakshiLexicon <105944865+MeenakshiLexicon@users.noreply.github.com>
2022-09-10 18:00:35 +0200

    Delete Lektion-9 directory

--- Lektion-9/01_EntityFrameworkCore/Models/ViewModels/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace _01_EntityFrameworkCore.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- Lektion-9/02_EntityFrameworkCore/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace _02_EntityFrameworkCore.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
