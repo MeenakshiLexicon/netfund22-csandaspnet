aa3afdc80adc8cfbc7b95e684390aa9931aaf3ab
MeenakshiLexicon <105944865+MeenakshiLexicon@users.noreply.github.com>
2022-09-10 17:59:45 +0200

    Delete Lektion-6 directory

--- Lektion-6/WebApi/Models/CreateProduct.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WebApi.Models
-{
-    public class CreateProduct
-    {
-        public string Name { get; set; } = null!;
-        public decimal Price { get; set; }
-    }
-}
--- Lektion-6/WebApi_Minimal/Models/Product.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApi_Minimal.Models
-{
-    public class Product
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = null!;
-        public decimal Price { get; set; }
-    }
-}
