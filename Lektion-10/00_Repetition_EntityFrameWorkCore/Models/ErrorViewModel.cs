bdfdcc5c4fbea927d5010781117d27f449967a97
MeenakshiLexicon <105944865+MeenakshiLexicon@users.noreply.github.com>
2022-09-10 17:58:58 +0200

    Delete Lektion-10 directory

--- Lektion-10/00_Repetition_EntityFrameWorkCore/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace _00_Repetition_EntityFrameWorkCore.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
